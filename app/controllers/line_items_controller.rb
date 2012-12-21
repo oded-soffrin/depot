00964312dd4bdb9827a3e8026c948061a34d1222
Oded Soffrin <oded@momentoro.com>
2012-12-21 03:54:42 +0200

    Chapter 11

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,11 +42,11 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product_id => product.id)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, :notice => 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart}
         format.json { render :json => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
@@ -75,11 +75,16 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1.json
   def destroy
     @line_item = LineItem.find(params[:id])
+    cart = @line_item.cart.remove_product(@line_item.id)
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to cart_url(cart.id) }
       format.json { head :no_content }
     end
   end
+
+
+
+
 end
